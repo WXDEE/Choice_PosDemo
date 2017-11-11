4c1c323b0c440a9444d05003950521fad5079a1e
KnoveZ <308346293@qq.com>
2017-11-12 01:26:37 +0800

    OrderInfo订单详情页面Update

--- 前端/reactdemo/src/components/pages/FoodInfo.jsx
+++ 前端/reactdemo/src/components/pages/FoodInfo.jsx
@@ -2,12 +2,13 @@
  * Created by Knove on 2017/11/9.
  */
 import React from 'react';
-
+import InfoTab from './InfoComponents/InfoTab';
 
 class FoodInfo extends React.Component{
     render(){
         return(
-            <div className="demo">
+            <div >
+                <InfoTab infoNum="2" />
               后台运维页面-菜品信息
             </div>
         )
--- 前端/reactdemo/src/components/pages/OrderInfo.jsx
+++ 前端/reactdemo/src/components/pages/OrderInfo.jsx
@@ -2,13 +2,11 @@
  * Created by Knove on 2017/11/9.
  */
 import React from 'react';
-import { Button} from 'antd';
+import { Button,Input,DatePicker ,Table,Icon} from 'antd';
 import { addNum } from '../../action/action';
 import { connect } from 'react-redux'; // 引入connect
-
-
+import InfoTab from './InfoComponents/InfoTab';
 class OrderInfo extends React.Component{
-
     constructor(props) {
         super(props);
         this.state = {
@@ -21,14 +19,105 @@ class OrderInfo extends React.Component{
     }
     render(){
         const theNumber = this.props.dataA;
+        function onChange(date, dateString) {
+            console.log(date, dateString);
+        }
+        const columns = [{
+            title: '订单编号',
+            dataIndex: 'ID',
+            key: 'id',
+        }, {
+            title: '下单时间',
+            dataIndex: 'Time',
+            key: 'time',
+        }, {
+            title: '菜品数量',
+            dataIndex: 'FoodNum',
+            key: 'foodnum',
+        },  {
+            title: '桌号',
+            dataIndex: 'DeskID',
+            key: 'deskID',
+        }, {
+            title: '消费金额',
+            dataIndex: 'Money',
+            key: 'money',
+            render: text => <span>{text}$</span>,
+        }, {
+            title: '订单状态',
+            dataIndex: 'State',
+            key: 'state',
+        }, {
+            title: '操作',
+            dataIndex: 'Func',
+            key: 'func',
+            render: text => <a href="#">查看明细</a>,
+        }
+    ];
+
+        const data = [{
+            key: '1',
+            ID: '0H234',
+            Time: '2017-11-11  10:00',
+            FoodNum: '12',
+            DeskID: '2',
+            Money: '323',
+            State:'待付款',
+        },{
+            key: '2',
+            ID: '0H234',
+            Time: '2017-11-11  10:00',
+            FoodNum: '12',
+            DeskID: '1',
+            Money: '323',
+            State:'已下单',
+        },{
+            key: '3',
+            ID: '0H234',
+            Time: '2017-11-11  10:00',
+            FoodNum: '12',
+            DeskID: '5',
+            Money: '323',
+            State:'已结账',
+        },{
+            key: '4',
+            ID: '0H234',
+            Time: '2017-11-11  10:00',
+            FoodNum: '12',
+            DeskID: '4',
+            Money: '323',
+            State:'已结账',
+        }];
         return(
             <div>
-                OrderInfo --
+             {/*   OrderInfo --
                 <span>{theNumber}</span>
                 <Button type="primary" onClick={this.add}>Add</Button>
                 <div className="demo">
                     后台运维页面-订单明细
+                </div>*/}
+                <InfoTab infoNum="1" />
+                <section className="funcTitle">
+                    <div className="normalInput">订单编号 <Input  className="datePicker" /></div>
+                    <div className="normalInput">下单时间
+                        <DatePicker onChange={onChange} className="datePicker" />
+                        至<DatePicker onChange={onChange} className="datePicker" />
+                    </div>
+                    <div className="normalInput"><Button type="primary" >查询</Button></div>
+
+                </section>
+                <div className="tableMain">
+                    <span className="tableDate">营业额：<span className="tableMoney">250,000.00¥</span></span>
+                    <span className="tableDate">| </span>
+                    <span className="tableDate">订单量：<span className="tableMoney">50,000.00笔</span></span>
+                    <Table
+                        columns={columns}
+                        dataSource={data}
+                        className=""
+                        size="small"
+                    />
                 </div>
+
             </div>
         )
     }
