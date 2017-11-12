0a811ccae32dfbd8a11f5ad5c0eea7e178a264bc
luhao <luhao199611@163.com>
2017-11-12 19:10:48 +0800

    redis
    redis

--- 后端/pos/src/main/java/com/choice/service/StudentService.java
+++ 后端/pos/src/main/java/com/choice/service/StudentService.java
@@ -7,16 +7,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.choice.entity.Student;
+import com.choice.mapper.JedisClient;
 import com.choice.mapper.StudentMapper;
 
 @Service
 public class StudentService {
 	@Autowired
 	private StudentMapper studentMapper;
+	@Autowired JedisClient jedisClient;
 	
 	public List<Student> selectAll(){
 		List<Student> listStudent = studentMapper.getAllStu();
 		System.out.println(listStudent);
+		jedisClient.set("list", "success");
+		System.out.println(jedisClient.get("list"));
 		return listStudent;
 		
 	}
