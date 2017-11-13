83a7210cef8725490a509a6cc60b63de8caab3ee
xi329 <501271665@qq.com>
2017-11-13 10:58:02 +0800

    菜品编号修改成菜品id

--- 后端/pos/src/main/java/com/choice/entity/OrderItem.java
+++ 后端/pos/src/main/java/com/choice/entity/OrderItem.java
@@ -5,9 +5,49 @@ public class OrderItem {
 	private Integer id;
 	private String oiNum;//订单明细编号
 	private String oNum;//订单编号
-	private String dNum;//菜品编号
+	private String dId;//菜品id
 	private String oiCount;//单一菜品数量
 	private String oiStatus;//订单菜品状态 0：未上菜 1：已上菜
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getOiNum() {
+		return oiNum;
+	}
+
+	public void setOiNum(String oiNum) {
+		this.oiNum = oiNum;
+	}
+
+	public String getoNum() {
+		return oNum;
+	}
+
+	public void setoNum(String oNum) {
+		this.oNum = oNum;
+	}
+
+	public String getdId() {
+		return dId;
+	}
+
+	public void setdId(String dId) {
+		this.dId = dId;
+	}
+
+	public String getOiCount() {
+		return oiCount;
+	}
+
+	public void setOiCount(String oiCount) {
+		this.oiCount = oiCount;
+	}
 
 	public String getOiStatus() {
 		return oiStatus;
@@ -16,47 +56,4 @@ public class OrderItem {
 	public void setOiStatus(String oiStatus) {
 		this.oiStatus = oiStatus;
 	}
-
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getOiNum() {
-		return oiNum;
-	}
-	public void setOiNum(String oiNum) {
-		this.oiNum = oiNum;
-	}
-	public String getoNum() {
-		return oNum;
-	}
-	public void setoNum(String oNum) {
-		this.oNum = oNum;
-	}
-	public String getdNum() {
-		return dNum;
-	}
-	public void setdNum(String dNum) {
-		this.dNum = dNum;
-	}
-	public String getOiCount() {
-		return oiCount;
-	}
-	public void setOiCount(String oiCount) {
-		this.oiCount = oiCount;
-	}
-	public OrderItem(Integer id, String oiNum, String oNum, String dNum,
-			String oiCount) {
-		this.id = id;
-		this.oiNum = oiNum;
-		this.oNum = oNum;
-		this.dNum = dNum;
-		this.oiCount = oiCount;
-	}
-	public OrderItem() {
-	}
-	
-	
 }
