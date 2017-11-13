c0a9317ba8678e979b12520b8480ba56679e2f88
luhao <luhao199611@163.com>
2017-11-13 19:33:14 +0800

    解决maven运行错误

--- 后端/pos/src/main/java/com/choice/mapper/StudentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.choice.mapper;
-
-import java.util.List;
-
-import com.choice.entity.Student;
-
-public interface StudentMapper {
- List<Student> getAllStu();
-}
--- 后端/pos/src/main/java/com/choice/service/impl/DeskServiceImpl.java
+++ 后端/pos/src/main/java/com/choice/service/impl/DeskServiceImpl.java
@@ -2,11 +2,18 @@ package com.choice.service.impl;
 
 import com.choice.common.ServerResponse;
 import com.choice.entity.Desk;
+import com.choice.mapper.DeskMapper;
 import com.choice.service.DeskService;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
 public class DeskServiceImpl implements DeskService {
+	@Autowired
+	private DeskMapper deskMapper;
+	
     public ServerResponse<List<Desk>> queryAllDesk() {
         return null;
     }
