e680fd9551b59079aa0dbf9993d90019d67d4fd6
luhao <luhao199611@163.com>
2017-11-14 11:11:35 +0800

    新增ActiveMQ

---
+++ 后端/pos/src/main/java/com/choice/filter/QueueMessageListener.java
@@ -0,0 +1,24 @@
+package com.choice.filter;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class QueueMessageListener implements MessageListener{
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		TextMessage tm = (TextMessage) message;
+        try {
+            System.out.println("QueueMessageListener监听到了文本消息：\t"
+                    + tm.getText());
+            //do something ...
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+		
+	}
+
+}
---
+++ 后端/pos/src/test/java/com/choice/service/MQServiceTest.java
@@ -0,0 +1,25 @@
+package com.choice.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/applicationContext-*.xml"})
+public class MQServiceTest {
+	@Autowired 
+	private MQService mqService;
+	@Test
+	public void testSendMessage() {
+		mqService.sendMessage("hello");
+	}
+
+	@Test
+	public void testReceive() {
+		
+	}
+
+}
