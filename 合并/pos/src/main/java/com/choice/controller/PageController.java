bcabc744919a319f34196acf41bbb39be9857f98
luhao <luhao199611@163.com>
2017-11-15 20:58:14 +0800

    8080/访问

---
+++ 合并/pos/src/main/java/com/choice/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.choice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+
+}
